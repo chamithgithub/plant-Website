574a06f64f5327671d5fe834cec64545cb18dc72
A.C.Dilshan <dchamith4@gmail.com>
2025-09-10 14:17:00 +0530

    about add

---
+++ src/app/about/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Link from 'next/link'
+
+function Page() {
+  return (
+    <div>
+      <h1>about</h1>
+      <Link href="/">back to home</Link>
+    </div>
+  )
+}
+
+export default Page
